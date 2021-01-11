cb185940a486f374c703f8e82084fb0857b56353
SaulSantiago <paperspin7@gmail.com>
2021-01-11 18:38:19 -0500

    add player code

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pygame
 
-class Pong:
-    HEIGHT: int = 800
-    WIDTH: int = 1600
-
+class Board():
     def __init__(self):
-        pygame.init() #start the pygame instance
-        #setup the screen
-        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.init()
+
+        self.display = pygame.display.set_mode((0,0), pygame.fullscreen)
         self.clock = pygame.time.Clock()
-
-    def game_loop(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    print('Game Ending')
-                    return
-
-
-if __name__ == '__main__':
-    pong = Pong()
-    pong.game_loop()
+    def run(self):
+        done = False
+        player_1 = Paddle()
+        player_2 = Paddle()
+        player_1_score = 0
+        player_2_score = 0
+        max_score = 3
+        game_history = []
+        while not done:
+            pass
+class Paddle():
+    pass
+class Ball():
+    pass
+class ScoreBoard():
+    pass
+if __name__ =='__main__':
+    pass
